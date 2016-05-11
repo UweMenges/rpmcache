158dcece4f968f685443aa56bf444df375685522
Uwe Menges <uwe.menges@web.de>
2016-05-11 23:34:49 +0200

    remove unused rpm metadata files

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # dnf repomanag can't handle rpm in subdirs properly, so we need to call
 # it separately for every dir containing .rpm files
-for dir in $(find . -name \*.rpm -exec dirname {} + | sort -u); do
+for dir in $(find . -type f -name \*.rpm -exec dirname {} + | sort -u); do
     rpmfile=($(dnf repomanage -o "$dir"))
     [ -n "${rpmfile[0]}" ] && rm -v ${rpmfile[@]}
+done
+
+for dir in $(find . -type d -name repodata); do
+    [ ! -e "$dir/repomd.xml" ] && continue
+    cd "$dir" && { for file in *.gz *.xz; do
+	if grep "$file" repomd.xml; then
+	    echo "keep $file"
+	else
+	    rm "$file" 2> /dev/null
+	fi
+    done; cd -; }
 done
