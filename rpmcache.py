cc10c0ea44f04eb17ff3b7aa93417e45e4fac7e6
Uwe Menges <uwe.menges@web.de>
2017-03-18 19:02:01 +0100

    enable HTTP redirect following (eg. http -> https)

--- rpmcache.py
+++ rpmcache.py
@@ -100,6 +100,7 @@ def get_url(url):
     log('D: [%d] downloading %s to %s' % (mypid, url, dest))
     curl = pycurl.Curl()
     curl.setopt(curl.URL, url)
+    curl.setopt(curl.FOLLOWLOCATION, True)
     path = '/'.join(dest.split('/')[:-1])
     if not os.path.exists(path):
         # parallel download of rpms in subdir will create it right now
