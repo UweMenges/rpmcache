700c99f6b0af36935b179a5f623ae7fc130592e1
Uwe Menges <uwe.menges@web.de>
2017-04-21 00:50:46 +0200

    improve output

--- cleanup.sh
+++ cleanup.sh
@@ -5,24 +5,25 @@
 # it separately for every dir containing .rpm files
 for dir in $(find . -type f -name \*.rpm -exec dirname {} + | sort -u); do
     rpmfile=($(dnf repomanage -o "$dir"))
-    [ -n "${rpmfile[0]}" ] && rm -v ${rpmfile[@]}
+    [ -n "${rpmfile[0]}" ] && rm -v "${rpmfile[@]}"
 done
 
+shopt -s nullglob
 for dir in $(find . -type d -name repodata); do
     [ ! -e "$dir/repomd.xml" ] && continue
     cd "$dir" && { for file in *.gz *.xz; do
-	if grep "$file" repomd.xml; then
+	if grep "$file" repomd.xml >/dev/null; then
 	    echo "keep $file"
 	else
-	    rm "$file" 2> /dev/null
+	    rm -v "$file"
 	fi
-    done; cd -; }
+    done; cd - >/dev/null; }
 done
 
 # remove 404 html files we got from old metadata queries
-find /var/cache/rpmcache/ -type f -size -2k -exec file {} + \
+files=$(find /var/cache/rpmcache/ -type f -size -2k -exec file {} + \
     | grep HTML \
     | while read file rest; do
 	echo ${file%:}
-      done \
-    | xargs rm -v
+      done)
+[ -n "$files" ] && rm -v "$files"
